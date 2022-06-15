ac681e3790c9d10b31ae990495642def427a945b
Mount-Blanc <mountblanc87@gmail.com>
2022-06-15 10:18:58 -0700

    created card componenet

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Card from './Components/Card';
 function App() {
   return (
     <div className="App">
@@ -18,6 +18,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Card>Card</Card>
     </div>
   );
 }
