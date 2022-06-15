2b87998ad27ee843ab3186d0b2e62803d2693025
Mount-Blanc <mountblanc87@gmail.com>
2022-06-15 10:24:15 -0700

    added input to componet Card

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,6 +2,16 @@ import React from "react";
 import './card.css';
 
 function Card() {
-    return <div> Card</div>
+    return <div> 
+        <div>
+            <form>
+                <label>To do </label>
+            <input type="text" ></input>
+            <label>Due date</label>
+            <input type="date"></input>
+
+            </form>
+        </div>
+    </div>
 }
 export default Card;
