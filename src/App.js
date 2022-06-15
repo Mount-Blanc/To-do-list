189493a42b0c811a7eb64279f01f152875869f61
Mount-Blanc <mountblanc87@gmail.com>
2022-06-15 10:25:23 -0700

    deleted react logo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,6 @@ import Card from './Components/Card';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
       <Card>Card</Card>
     </div>
   );
