ad36e179ef71259b6363796578994f4a5d692abb
Mount-Blanc <mountblanc87@gmail.com>
2022-06-15 18:48:55 -0700

    added jsx structuture to tasks

--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -3,7 +3,15 @@ import './Form.css';
 function Tasks () {
     return(
         <div className="form">
-           <p>example</p>
+            <div>
+                <h2>ToDo</h2>
+            </div>
+            <div>
+                Insert Task
+            </div>
+            <div>
+                Due Date
+            </div>
         </div>
     )
 }
