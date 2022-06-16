a6f59dd6a561499b5fbec5e2f26eca7c6d8148c5
Mount-Blanc <mountblanc87@gmail.com>
2022-06-15 18:22:34 -0700

    added on change event

--- src/Components/Form.js
+++ src/Components/Form.js
@@ -2,11 +2,14 @@ import React from "react";
 import './Form.css';
 
 function Card() {
+    const todo = (event) => {
+        console.log(event.target.value);
+    }
     return <div> 
         <div>
             <form className="style">
                 <label>To do </label>
-                <input type="text" ></input>
+                <input type="text" onChange={todo} ></input>
                 <label>Due date</label>
                 <input type="date"></input>
 
