d797a3dba728a562031fd1e77cbb485005000291
Mount-Blanc <mountblanc87@gmail.com>
2022-06-18 15:47:05 -0700

    created task items folder

---
+++ src/Components/Taskitems.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Taskitems () {
+
+
+
+
+}
+
+export default Taskitems
--- src/Components/Tasks.js
+++ src/Components/Tasks.js
@@ -1,17 +1,17 @@
 import './Tasks.css';
 import Card from './Card';
-
+import Form from './Form';
 function Tasks (props) {
 
     return(
         <Card>
-{props.items.map(dummyTodo => <Card task={dummyTodo.task}/>)} 
+{props.items.map(dummyTodo => <Form task={dummyTodo.task} date={dummyTodo.date} />)} 
         <div className="todo-title"> 
 
              <div className="todo-button" >Todo</div>
             
         
-             <div className="todo-description">
+             <div className="todo-description">{}
             </div>
         
              <div>{props.date}</div>
