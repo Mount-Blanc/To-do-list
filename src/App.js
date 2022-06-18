48d668503e434adc6214a1040a5e0d674abcdbbb
Mount-Blanc <mountblanc87@gmail.com>
2022-06-17 17:20:21 -0700

    Changed document title

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Form from './Components/Form';
 import Tasks from './Components/Tasks';
 import Card from './Components/Card';
+import {Helmet} from "react-helmet";
 
 function App() {
   const [newtask, setnewtask] = useState(' ')
@@ -16,7 +17,15 @@ const userdata = (newtask) => {
 
 
   return (
+
+
     <div className="App">
+      <Helmet>
+          <title>What to Do?</title>
+          <meta name="What to Do?" content="Todo list" />
+       </Helmet>
+
+
       <Card>
       <Form value= "" onUser={userdata} ></Form>
       </Card>
