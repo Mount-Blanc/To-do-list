0377d303b044d1140871726bc9d3abe608eaa6ea
Mount-Blanc <mountblanc87@gmail.com>
2022-06-20 13:28:24 -0700

    set up dynamic list rendering in tasks.js

--- src/Components/Expenses/Taskitems.js
+++ src/Components/Expenses/Taskitems.js
@@ -1,11 +1,17 @@
 import React from "react";
 import "./Taskitems.css";
 import Card from '../UI/Card';
+import Tasks from './Tasks';
 
 function Taskitems (props) {
 
 return ( 
+    <div>
+        <Card>
+        {props.items.map(dummyTodoData => <Tasks task = {dummyTodoData.task} duedate={dummyTodoData.duedate} />)}
 
+        </Card>
+    </div>
 )
 
 }
