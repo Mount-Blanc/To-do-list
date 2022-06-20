f17c010b0f61e884fac3e1e228a3a1af3c83e0c5
Mount-Blanc <mountblanc87@gmail.com>
2022-06-20 13:14:49 -0700

    created tasks js and css

--- src/Components/Expenses/Taskitems.js
+++ src/Components/Expenses/Taskitems.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Taskitems.css";
 import Card from '../UI/Card';
+import NewTask from '../NewTask/NewTask';
 
 function Taskitems (props) {
 
 return ( 
 <Card>
+    {props.items.map(dummyTodoData => <NewTask task={dummyTodoData.task} duedate = {dummyTodoData.date} />)}
 <div className="todo-title">
     <div className="todo-button" >Todo</div>
         <div className="todo-description"> {props.items.task}
